29f56f8603b10c238daa20e52f7397a004a6bdcc
Katherine Walendzik <katherinewalendzik@gmail.com>
2021-03-12 16:44:56 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -22,6 +22,7 @@ def valid_move?(board, index)
   if index.between?(0,8) && position_taken?(board, index) == false
     return true
   end
+end
   
 def move(board, index, value = "X")
   board[index] = value
